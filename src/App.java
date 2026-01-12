42a4940f0bf53600fb3c85e90262fac7062a8eb4
Boris <bpuente@gmail>
2026-01-12 16:41:21 -0500

    first commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int value = Integer.hashCode(20);   
+        System.out.println(value);
     }
 }
